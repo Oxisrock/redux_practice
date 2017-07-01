ff60e693b55551e10ee4627d5c7ed76a7e5a1f69
oxisrock <franciscoaular18@gmail.com>
2017-07-01 15:53:18 -0430

    Mejorando en react problemas en funciones indefinidas

--- src/pages/Location.js
+++ src/pages/Location.js
@@ -1,25 +1,27 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-@connect((store) => {
-  return {
-    user: store.user.user,
-    userFetched: store.user.fetched,
-    tweets: store.tweets.tweets.tweets
-  }
-})
+import { fetchUser } from '../actions/userActions'
 class Location extends Component {
+  componentWillMount () {
+    this.props.dispatch(fetchUser())
+  }
   render () {
-    console.log(this.props)
+    const { users } = this.props.user.users
+    console.log(users)
+    const mappedUsers = users.map(users => <li>{users.name}, {users.username}, {users.age} </li>)
     return (
       <div className=' container jumbotron'>
         <h2 className='text-center text-shadow '> Users List </h2>
         <ul>
-          <li>
-
-          </li>
+          {mappedUsers}
         </ul>
       </div>
     )
   }
 }
-export default Location
+export default connect((store) => {
+  return {
+    user: store.user.user,
+    userFecthed: store.user.fetched
+  }
+})(Location)
